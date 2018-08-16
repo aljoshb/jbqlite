e923ae6b81c3989b31703f8afa565e8a43cea026
joshuaalawode <joshalawode@gmail.com>
2018-08-15 23:54:52 -0400

    Bug fixed, needed proper brackets around #define values

--- include/table.h
+++ include/table.h
@@ -8,19 +8,19 @@
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
-#define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 255
-#define ID_SIZE size_of_attribute(Row, id)
-#define USERNAME_SIZE size_of_attribute(Row, username)
-#define EMAIL_SIZE size_of_attribute(Row, email)
-#define ID_OFFSET 0
-#define USERNAME_OFFSET ID_OFFSET+ID_SIZE
-#define EMAIL_OFFSET USERNAME_OFFSET+USERNAME_SIZE
-#define ROW_SIZE ID_SIZE+USERNAME_SIZE+EMAIL_SIZE
-#define PAGE_SIZE 4096
-#define TABLE_MAX_PAGES 100
-#define ROWS_PER_PAGE PAGE_SIZE/ROW_SIZE
-#define TABLE_MAX_ROWS ROWS_PER_PAGE*TABLE_MAX_PAGES
+#define COLUMN_USERNAME_SIZE          32
+#define COLUMN_EMAIL_SIZE             255
+#define ID_SIZE                       size_of_attribute(Row, id)
+#define USERNAME_SIZE                 size_of_attribute(Row, username)
+#define EMAIL_SIZE                    size_of_attribute(Row, email)
+#define ID_OFFSET                     0
+#define USERNAME_OFFSET               (ID_OFFSET+ID_SIZE)
+#define EMAIL_OFFSET                  (USERNAME_OFFSET+USERNAME_SIZE)
+#define ROW_SIZE                      (ID_SIZE+USERNAME_SIZE+EMAIL_SIZE)
+#define PAGE_SIZE                     4096
+#define TABLE_MAX_PAGES               100
+#define ROWS_PER_PAGE                 (PAGE_SIZE/ROW_SIZE)
+#define TABLE_MAX_ROWS                (ROWS_PER_PAGE*TABLE_MAX_PAGES)
 
 struct Row_t {
     uint32_t id;
