09eb5730246d171e7f926a24108bc6b53754c21b
joshuaalawode <joshalawode@gmail.com>
2018-08-19 16:51:57 -0400

    Implemented the ability to split a leaf node, when full

--- include/table.h
+++ include/table.h
@@ -32,10 +32,20 @@
 #define LEAF_NODE_KEY_SIZE            sizeof(uint32_t)
 #define LEAF_NODE_KEY_OFFSET          0
 #define LEAF_NODE_VALUE_SIZE          ROW_SIZE
-#define LEAF_NODE_VALUE_OFFSET        (LEAF_NODE_KEY_OFFSET+LEAF_NODE_KEY_SIZE)
-#define LEAF_NODE_CELL_SIZE           (LEAF_NODE_KEY_SIZE+LEAF_NODE_VALUE_SIZE)
-#define LEAF_NODE_SPACE_FOR_CELLS     (PAGE_SIZE-LEAF_NODE_HEADER_SIZE)
-#define LEAF_NODE_MAX_CELLS           (LEAF_NODE_SPACE_FOR_CELLS/LEAF_NODE_CELL_SIZE)
+#define LEAF_NODE_VALUE_OFFSET              (LEAF_NODE_KEY_OFFSET+LEAF_NODE_KEY_SIZE)
+#define LEAF_NODE_CELL_SIZE                 (LEAF_NODE_KEY_SIZE+LEAF_NODE_VALUE_SIZE)
+#define LEAF_NODE_SPACE_FOR_CELLS           (PAGE_SIZE-LEAF_NODE_HEADER_SIZE)
+#define LEAF_NODE_MAX_CELLS                 (LEAF_NODE_SPACE_FOR_CELLS/LEAF_NODE_CELL_SIZE)
+#define LEAF_NODE_RIGHT_SPLIT_COUNT         ((LEAF_NODE_MAX_CELLS+1)/2)
+#define LEAF_NODE_LEFT_SPLIT_COUNT          (LEAF_NODE_MAX_CELLS+1 - LEAF_NODE_RIGHT_SPLIT_COUNT)
+#define INTERNAL_NODE_NUM_KEYS_SIZE         sizeof(uint32_t)
+#define INTERNAL_NODE_NUM_KEYS_OFFSET       COMMON_NODE_HEADER_SIZE
+#define INTERNAL_NODE_RIGHT_CHILD_SIZE      sizeof(uint32_t)
+#define INTERNAL_NODE_RIGHT_CHILD_OFFSET    (INTERNAL_NODE_NUM_KEYS_OFFSET + INTERNAL_NODE_NUM_KEYS_SIZE)
+#define INTERNAL_NODE_HEADER_SIZE           (COMMON_NODE_HEADER_SIZE + INTERNAL_NODE_NUM_KEYS_SIZE + INTERNAL_NODE_RIGHT_CHILD_SIZE)
+#define INTERNAL_NODE_KEY_SIZE              sizeof(uint32_t)
+#define INTERNAL_NODE_CHILD_SIZE            sizeof(uint32_t)
+#define INTERNAL_NODE_CELL_SIZE             (INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE)
 
 struct Row_t {
     uint32_t id;
@@ -81,6 +91,17 @@ void serialize_row(Row* source, void* destination);
 void deserialize_row(void* source, Row* destination);
 void* cursor_value(Cursor* cursor);
 void print_row(Row* row);
+bool is_node_root(void* node);
+void set_node_root(void* node, bool is_root);
+uint32_t* internal_node_num_keys(void* node);
+uint32_t* internal_node_right_child(void* node);
+uint32_t* internal_node_cell(void* node, uint32_t cell_num);
+uint32_t* internal_node_child(void* node, uint32_t child_num);
+uint32_t* internal_node_key(void* node, uint32_t key_num);
+uint32_t get_unused_page_num(Pager* pager);
+uint32_t get_node_max_key(void* node);
+void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value);
+void create_new_root(Table* table, uint32_t right_child_page_num);
 Cursor* table_start(Table* table);
 Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key);
 Cursor* table_find(Table* table, uint32_t key);
@@ -91,8 +112,10 @@ void* leaf_node_cell(void* node, uint32_t cell_num);
 uint32_t* leaf_node_key(void* node, uint32_t cell_num);
 void* leaf_node_value(void* node, uint32_t cell_num);
 void initialize_leaf_node(void* node);
+void initialize_internal_node(void* node);
 void cursor_advance(Cursor* cursor);
 void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value);
-void print_leaf_node(void* node);
+void indent(uint32_t level);
+void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level);
 
 #endif
